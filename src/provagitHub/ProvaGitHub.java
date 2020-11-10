302e16a77d4eb13388b8539f987b3a8c7a07b7a8
arrigoni.alessandro <arrigoni.alessandro@TAVE-PC09.OSDBSESTO.local>
2020-11-10 13:29:41 +0100

    edit while with exit condition

---
+++ src/provagitHub/ProvaGitHub.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provagitHub;
+
+/**
+ *
+ * @author arrigoni.alessandro
+ */
+public class ProvaGitHub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int i=2; 
+    while(i < 100){
+    System.out.println("ehi ciao"+i);
+    i=i++;    
+    }
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
